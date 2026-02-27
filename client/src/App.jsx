ff2507a431558421f734ddfee093f9d8dd377c56
danishhkhann <danishalikhn@gmail.com>
2026-02-28 04:02:07 +0530

    Commenr removed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -12,7 +12,7 @@ function App() {
   const [error, setError] = useState(null);
   const [isDialogOpen, setIsDialogOpen] = useState(false);
 
-  //Hi danish
+  //Fetch Tasks
   useEffect(() => {
     fetchTasks();
   }, []);
