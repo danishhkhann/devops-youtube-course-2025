b51ae46d2ca965b1724f44431205f43a1f4d97ba
danishhkhann <danishalikhn@gmail.com>
2026-02-28 03:35:27 +0530

    trial

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -12,7 +12,7 @@ function App() {
   const [error, setError] = useState(null);
   const [isDialogOpen, setIsDialogOpen] = useState(false);
 
-  // Fetch tasks on component mount
+  //Hi danish
   useEffect(() => {
     fetchTasks();
   }, []);
